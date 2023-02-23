3f167b659ee9fa0e735161bbb229b95c44a0c305
root <favourchinazaj@gmail.com>
2023-02-23 06:05:05 -0800

    task 0 commit

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,3 +1,16 @@
 #includ "main.h"
 /**
- * positive_or_negative - 
+ * positive_or_negative - checks for positive or negative numbers
+ *
+ * @i: the number to be checked
+ *
+ * Return: always 0
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative", i);
+	else
+		printf("%d is positive", i);
+}
