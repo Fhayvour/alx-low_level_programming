880d29e8b2c3ba5c62eacc69a9962a0973317671
root <favourchinazaj@gmail.com>
2023-03-16 06:58:17 -0700

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -13,11 +13,9 @@
 int is_digit(char *s)
 {
 	int i = 0;
-
 	while (s[i])
 	{
 		if (s[i] < '0' || s[i] > '9')
-
 			return (0);
 		i++;
 	}
@@ -34,12 +32,10 @@ int is_digit(char *s)
 int _strlen(char *s)
 {
 	int i = 0;
-
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-
 	return (i);
 }
 
@@ -50,7 +46,6 @@ int _strlen(char *s)
 void errors(void)
 {
 	printf("Error\n");
-
 	exit(98);
 }
 
@@ -65,7 +60,6 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
@@ -82,53 +76,33 @@ int main(int argc, char *argv[])
 	result = malloc(sizeof(int) * len);
 
 	if (!result)
-
 		return (1);
 
 	for (i = 0; i <= len1 + len2; i++)
-
 		result[i] = 0;
-
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
-
 		carry = 0;
-
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-
 			digit2 = s2[len2] - '0';
-
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
 
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
-
 		}
-
 		if (carry > 0)
-
-			result[len1 + len2 + 1] += carry;
-	}
+	result[len1 + len2 + 1] += carry;	}
 	for (i = 0; i < len - 1; i++)
-	{
-		if (result[i])
-
-			a = 1;
-
-		if (a)
-
-			_putchar(result[i] + '0');
+{
+	if (result[i])
+		a = 1;
+	if (a)
+	_putchar(result[i] + '0');
 	}
-
 	if (!a)
-
-		_putchar('0');
-
+	_putchar('0');
 	_putchar('\n');
-
 	free(result);
-
-	return (0);
-}
+	return (0); }
