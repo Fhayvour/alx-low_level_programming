4dce5b0261cdf122af69d8d56617f8138b00b5b1
root <favourchinazaj@gmail.com>
2023-03-16 12:56:46 -0700

    ♡

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,15 +1,13 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-#define ERR_MSG "Error"
+#include "main.h"
 
 /**
- * is_digit - checks if a string contains a non-digit char
- * @s: string to be evaluated
- *
- * Return: 0 if a non-digit is found, 1 otherwise
- */
-
+* is_digit - checks if a string contains a non-digit char
+* @s: string to be evaluated
+*
+* Return: 0 if a non-digit is found, 1 otherwise
+*/
 int is_digit(char *s)
 {
 	int i = 0;
@@ -24,12 +22,11 @@ int is_digit(char *s)
 }
 
 /**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
- */
-
+* _strlen - returns the length of a string
+* @s: string to evaluate
+*
+* Return: the length of the string
+*/
 int _strlen(char *s)
 {
 	int i = 0;
@@ -40,22 +37,23 @@ int _strlen(char *s)
 	}
 	return (i);
 }
-/**
- * errors - handles errors for main
- */
 
+/**
+* errors - handles errors for main
+*/
 void errors(void)
 {
 	printf("Error\n");
 	exit(98);
 }
+
 /**
- * main - multiplies two positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: always 0 (Success)
- */
+* main - multiplies two positive numbers
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: always 0 (Success)
+*/
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
@@ -67,9 +65,7 @@ int main(int argc, char *argv[])
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
-
 	result = malloc(sizeof(int) * len);
-
 	if (!result)
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
@@ -84,19 +80,20 @@ int main(int argc, char *argv[])
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
-}
-	if (carry > 0)
-	result[len1 + len2 + 1] += carry;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
 	for (i = 0; i < len - 1; i++)
-{
-	if (result[i])
-	a = 1;
-	if (a)
-	_putchar(result[i] + '0');
-}
-	if (!a);
-	_putchar('0');
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
 	_putchar('\n');
 	free(result);
-	return (0); }
+	return (0);
+}
